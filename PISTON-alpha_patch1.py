f376f52a6a0bc19eaa941e0bc93f6944d10156f2
classiccatlinux <classiccatlinux@gmail.com>
2026-05-10 20:10:53 -0400

    Update PISTON-alpha_patch1.py

--- PISTON-alpha_patch1.py
+++ PISTON-alpha_patch1.py
@@ -1,26 +1,35 @@
+import time
+import os
+import sys
+
+# ============
+# == PISTON ==
+# ============
+# info:
+# alpha_patch1
+# FOSS - AGPL 3.0
+# classiccatlinux (randomnerd41)
+# status: updating.
 
 
+def update_debian_sudo():
+    os.system("sudo apt update -y && sudo apt upgrade -y")
 
+def clear():
+    if sys.platform == "win32":
+        os.system("cls")
+    else:
+        os.system("clear")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def wait(wt):
+    time.sleep(wt)
+    
+def osname():
+    name = sys.platform
+    print("Your OS is:", name)
+    time.sleep(3)
+    clear()
+    
 def cmd(c):
     os.system(c)
 
